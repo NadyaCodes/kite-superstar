6b07fb44b67cc19dae6ca75cc2fac054181c333d
NadyaCodes <nadyacorscadden@gmail.com>
2023-01-21 10:07:40 -0500

    change points size

--- src/Winner.js
+++ src/Winner.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function Winner(props) {
   return (
     <div className="winner">
-      <div>Winner! {props.points} points</div>
+      <div>Winner! </div>
+      <div className="points">{props.points} points</div>
       <div>{props.highScore === true && " 🏄 High Score! 🏄"}</div>
     </div>
   );
